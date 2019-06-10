1ce5871eb7eec6b24a9973b9d7ad24fb3b05d40b
Trym Lund Flogard <trym2001@hotmail.com>
2019-06-10 21:32:23 +0200

    obsolete

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -39,23 +39,4 @@ export class DashboardService {
     return status
   }
 
-  /** Return distinct message for sent, upload progress, & response events */
-  private getEventMessage(event: HttpEvent<any>) {
-    switch (event.type) {
-      case HttpEventType.Sent:
-        return `henter snmp resultater`;
-
-      case HttpEventType.DownloadProgress:
-        // Compute and show the % done:
-        const percentDone = Math.round(100 * event.loaded / event.total);
-        return `${percentDone}% uploaded.`;
-
-      case HttpEventType.Response:
-        return `Hentet`;
-
-      default:
-        return `tror noe gikk galt: ${event.type}.`;
-    }
-  }
-
 }
